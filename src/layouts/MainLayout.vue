29df580cc2a73e4c9d8e22e6959ca2775bee1404
Marcelo <marcelo_chociay@hotmail.com>
2021-04-16 17:36:16 -0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <router-link 
+          to="/"
+          v-slot="{ href, navigate }"
+        >
+          <a class="text-white" :href="href" @click="navigate">
+            My Notes
+          </a>
+        </router-link>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/pages/New.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-page padding>
+    <Container>
+      <h3>New Note</h3>
+      <form @submit="submit">
+        <q-input class="q-mt-sm" outlined v-model="note.title" label="Title" />
+
+        <q-input
+          class="q-mt-sm"
+          outlined
+          v-model="note.description"
+          label="Description"
+          dense
+        />
+
+        <q-card flat bordered class="q-mt-sm">
+          <q-editor v-model="note.content" min-height="5rem" />
+        </q-card>
+
+        <div class="q-mt-md">
+          <q-btn color="grey" to="/" type="reset">Cancel</q-btn>
+          <q-btn class="q-ml-sm" color="positive" type="submit">Save</q-btn>
+        </div>
+      </form>
+    </Container>
+  </q-page>
+</template>
+
+<script>
+import Container from 'src/components/Container.vue'
+import { defineComponent, reactive, computed } from 'vue'
+import { useLocalNotes } from 'src/helper'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+  components: { Container },
+  name: 'PageNew',
+
+  setup() {
+    const router = useRouter()
+    const notes = useLocalNotes()
+
+    const note = reactive({
+      title: '',
+      description: '',
+      content: ''
+    })
+
+    const submit = () => {
+      if (note.title === '' && note.description === '' && note.content === '') return
+
+      notes.value.unshift({
+        ...note,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      })
+
+      router.push('/')
+
+      note.title = ''
+      note.description = ''
+      note.content = ''
+    }
+
+    return { note, submit }
+  }
+})
+</script>
